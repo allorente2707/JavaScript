b9db76f5fef0985eb3074bae0c05432c19833554
allorente2707 <allorente2707@gmail.com>
2023-05-02 13:10:53 -0300

    agregar archivo js al repositorio local

---
+++ js/practico_und1.js
@@ -0,0 +1,352 @@
+//Actividad Práctica Adicional - JavaScript Unidad 1
+
+
+/*1. Escribe un programa de una sola línea que haga que aparezca en la pantalla un
+alert que diga “Hello World”.*/
+
+//console.log(alert("Hello World!!"))
+
+
+/*2. Escribe un programa de una sola línea que escriba en la pantalla un texto que
+diga “Hello World” (document.write).*/
+
+//document.write("Hello World!!");
+
+
+/*3. Escribe un programa de una sola línea que escriba en la pantalla el resultado
+de sumar 3 + 5.*/
+
+/*document.write(3+5);
+
+
+/*4. Escribe un programa de dos líneas que pida el nombre del usuario con un
+prompt y escriba un texto que diga “Hola nombreUsuario”.*/
+
+/*
+let nombre = prompt("Ingresar nombre de usuario por favor:")
+document.write("Hola", nombre)
+*/
+
+/*5. Escribe un programa de tres líneas que pida un número, pida otro número y
+escriba el resultado de sumar estos dos números.*/
+
+/*
+let num1 = prompt("Ingrese un numero:");
+let num2 = prompt("Ingrese otro numero:");
+document.write("La suma es: ", (parseInt(num1) + parseInt(num2)));*/
+
+/*6. Escribe un programa que pida dos números y escriba en la pantalla cual es el
+mayor.*/
+
+/*
+let num3 = prompt("Ingrese un numero:")
+let num4 = prompt("Ingrese otro numero:")
+if (num3 > num4) {
+    document.write(num3);
+} else {
+    document.write(num4);
+}
+*/
+
+/*7. Escribe un programa que pida 3 números y escriba en la pantalla el mayor de
+los tres.*/
+
+
+let n1 = prompt("Ingrese un numero:")
+let n2 = prompt("Ingrese otro numero:")
+let n3 = prompt("Ingrese un nuevo numero:")
+
+if (n1 > n2 && n1 > n3) {
+    document.write(n1);
+}   else if (n2 > n3) {
+    document.write(n2);
+}   else {
+    document.write(n3);
+}
+
+
+//8. Escribe un programa que pida un número y diga si es divisible por 2.
+
+/*
+let numero = prompt("Ingrese un numero:")
+if (numero%2==0) {
+    document.write(numero," es divisible por 2 o sea que es un numero par!")
+} else {
+    document.write(numero, " es no esdivisible por 2 o sea que es un numero impar!")
+}
+*/
+
+/*
+var n1 = prompt("Escribe un número");
+if (n1 % 2 === 0) {
+document.write("Es divisible por 2");
+} else {
+document.write("No es divisible por 2");
+}*/
+
+/*9. Escribe un programa que pida una frase y escriba cuantas veces aparece la
+letra a*/
+
+/*
+let frase = prompt("tirame una frase")
+let n = 0;
+
+for(let i=0; i<frase.length; i++) {
+    if(frase[i] == "a") {
+        n++;
+    }
+    i++;
+}
+
+document.write("La letra a aparece ",n," veces")
+*/
+
+
+
+//10. Escribe un programa que pida una frase y escriba las vocales que aparecen.
+
+/*
+let frase=prompt("Ingresar frase")
+let vocal=0;
+for(let j=0; j<frase.length; j++) {
+
+    if(frase[j] == "a" || frase[j] == "e" || frase[j] == "i" || frase[j] == "o"  || frase[j]=="u") {
+        vocal++;
+    }
+    j++;
+}
+
+console.log("Aparecen ",vocal," vocales"); */
+
+/*
+var text = prompt("Escribe una frase");
+var nText = text.length;
+var i;
+for (i = 0; i < nText; i++) {
+if (text.substr(i,1) === "a" || text.substr(i,1) === "e" || text.substr(i,1) === "i" || text.substr(i,1) === "o" || text.substr(i,1) === "u") {
+document.write(text.substr(i,1);
+}
+}*/
+
+
+
+/*11. Escribe un programa que pida una frase y escriba cuántas de las letras que
+tiene son vocales.*/
+
+/*
+let frase=prompt("Ingresar frase");
+
+let vocal=0;
+
+for(let j=0; j<frase.length; j++) {
+
+    if(frase[j] === "a" || frase[j] === "e" || frase[j] === "i" || frase[j] === "o"  || frase[j] ==="u") {
+        vocal++;
+    }
+    j++;
+}
+
+console.log("Aparecen ",vocal," vocales"); */
+
+
+
+/*12. Escribe un programa que pida una frase y escriba cuántas veces aparecen
+cada una de las vocales.*/
+
+
+
+
+
+
+/*13. Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o
+7 (sólo hay que comprobar si lo es por uno de los cuatro.*/
+
+/*
+let numero=prompt("Ingresar numero",);
+
+if (numero%2==0 || numero%3==0 || numero%5==0 || numero%7==0) {
+    console.log("Es divisible");
+}
+*/
+
+/*14. Añadir al ejercicio anterior que nos diga por cual de los cuatro es divisible (hay
+que decir todos por los que es divisible).*/
+
+/*
+let numero=prompt("Ingresar numero",);
+
+if (numero%2==0 ) {
+    console.log("Es divisible por 2");
+}
+
+if (numero%3==0 ) {
+    console.log("Es divisible por 3");
+}
+
+if (numero%5==0 ) {
+    console.log("Es divisible por 5");
+}
+
+if (numero%7==0 ) {
+    console.log("Es divisible por 7");
+}*/
+
+/*
+var n1 = prompt("Escribe un número");
+if (n1 % 2 === 0 || n1 % 3 === 0 || n1 % 5 === 0 || n1 % 7 === 0) {
+if (n1 % 2 === 0) {
+document.write("Es divisible por 2. ");
+}
+if (n1 % 3 === 0) {
+document.write("Es divisible por 3. ");
+}
+if (n1 % 5 === 0) {
+document.write("Es divisible por 5. ");
+}
+if (n1 % 7 === 0) {
+document.write("Es divisible por 7. ");
+}
+} else {
+document.write("No es divisible ni por 2, ni por 3, ni por 5, ni por 7");
+}*/
+
+/*15. Escribir un programa que escriba en pantalla los divisores de un número dado.*/
+
+/*
+let nro1 = prompt("Escribe un número");
+
+for (let i=2; i < nro1/2; i++) {
+
+  if (nro1%i == 0) {
+
+      document.write(i," ,");
+  }
+
+}*/
+
+
+
+/*16. Escribir un programa que escriba en pantalla los divisores comunes de dos
+números dados.*/
+
+/*var n1 = prompt("Escribe un número");
+var n2 = prompt("Escribe otro número");
+var menor;
+var i;
+if (n1 < n2) {
+menor = n1;
+} else {
+menor = n2;
+}
+for (i=2; i < menor / 2; i++) {
+if (n1 % i === 0 && n2 % i === 0) {
+document.write(i,", ");
+}
+}*/
+
+
+/*17. Escribir un programa que nos diga si un número dado es primo (no es divisible
+por ninguno otro número que no sea él mismo o la unidad).*/
+
+
+/*
+Solució 1
+var n1 = prompt("Escribe un número");
+var i;
+var primer = true;
+for (i=2; i < n1 / 2; i++) {
+if (n1 % i === 0) {
+primer = false;
+}
+}
+if (primer) {
+document.write("El número es primo");
+} else {
+document.write("El número no es primo");
+}
+
+Solució 2
+var n1 = prompt("Escribe un número");
+var i = 2;
+while (n1 % i !== 0 && i < n1 / 2 ) {
+i++;
+}
+if (n1 % i !== 0) {
+document.write("El número es primo");
+} else {
+document.write("El número no es primo");
+}*/
+
+
+/*18. Pide la edad y si es mayor de 18 años indica que ya puede conducir.*/
+
+
+/*19. Pide una nota (número). Muestra la calificación según la nota:
+● 0-3: Muy deficiente
+● 3-5: Insuficiente
+● 5-6: Suficiente
+● 6-7: Bien
+● 7-9: Notable
+● 9-10: Sobresaliente */
+
+
+/*20. Realiza un script que pida cadenas de texto hasta que se pulse “cancelar”. Al
+salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión´.*/
+
+
+/*21. Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un
+número deberá indicarse con un «alert» y seguir pidiendo. Al salir con “cancelar”
+deberá indicarse la suma total de los números introducidos.*/
+
+
+/*22. Realizar una página con un script que calcule el valor de la letra de un número
+de DNI (Documento Nacional de Identidad).
+El algoritmo para calcular la letra del dni es el siguiente :
+● El número debe ser entre 0 y 99999999
+● Debemos calcular el resto de la división entera entre el número y el número
+23.
+● Según el resultado, de 0 a 22, le corresponderá una letra de las siguientes:
+(T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E)
+● Si lo introducido no es un número deberá indicarse con un alert y volver a
+preguntar.
+● Deberá de repetirse el proceso hasta que el usuario pulse «cancelar».
+
+
+/*23. Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma : 
+1
+22
+333
+4444
+55555
+666666
+…….
+*/
+
+for (let i=2; i<=30; i++) {
+    document.write((i," ,"));
+}
+
+/*24. Haz un script que escriba una pirámide inversa de los números del 1 al
+número que indique el usuario de la siguiente forma : (suponiendo que indica 6).
+666666
+55555
+4444
+333
+22
+1
+*/
+
+
+/*25. Un script que escriba los números del 1 al 500, que indique cuáles son
+múltiplos de 4 y de 9 y que cada 5 líneas muestre una línea horizontal. Por ejemplo :
+1
+2
+3
+4 (Múltiplo de 4)
+5
+————————————————————
+6
+7
+8 (Múltiplo de 4)
+9 (Múltiplo de 9)
+10*/
---
+++ js/practico_und2.js
@@ -0,0 +1,423 @@
+//Tema: Funciones
+
+
+/*1. Realizar una función, a la que se le pase como parámetro un número N,
+y muestre por consola N veces, el siguiente mensaje: “Bienvenidos al
+curso Full Stack”*/
+
+function mostrar(n) {
+    
+    for (i=1; i<=n; i++) {
+        console.log("Bienvenidos al curso Full Stack"); 
+    }
+    
+}
+ mostrar(7)
+
+/*2. Diseñar una función que tenga como parámetros dos números, y que
+calcule el máximo.*/
+
+
+// Puedo ingrar numeros iguales!
+function max(nro1, nro2) {
+    if (nro1 > nro2) {
+        return nro1;
+       
+    } else {
+        if (nro2 > nro1) {
+            return nro2;
+        } 
+        return console.log("No hay maximo");
+    }
+    
+}
+console.log(max(100, 1))
+
+
+// Ingresar dos numero no iguales por favor. Gracias!
+function max2(nro1, nro2) {
+    if (nro1>nro2) {
+        return console.log(nro1);
+    }
+    return console.log(nro2);
+    
+}
+max2(17,34)
+
+
+//Si ingresas dos nemeros iguales pasa esto!
+max2(3,3)
+
+
+/*3. Crear una función que se llame promedio3, que reciba como
+parámetro tres valores y devuelva el promedio de los mismos.*/
+
+function promedio3(valor1, valor2, valor3) {
+    let promedio = (valor1+valor2+valor3)/3;
+    return console.log("El promedio de 3 estos tres numero es:", promedio);
+    
+}
+promedio3(30, 90, 60)
+
+
+function promedio32(valor1, valor2, valor3) {
+    return (valor1+valor2+valor3)/3;
+}
+console.log(promedio32(37, 87, 19));
+
+
+/*4. Crear una función que lea notas hasta que ingrese -1 y devuelve el
+promedio de las notas leídas. ( aunque no se suele leer valores en una
+función) Una función que se llame siguiente, que reciba como
+parámetro un valor entero, y devuelva el siguiente del número
+ingresado como parámetro.*/
+
+
+/*
+function promedio() {
+    let suma = 0;
+    let cantidad = 0;
+    let nota = parseInt(prompt("nota:", ));
+    while (nota != -1) {
+        suma+=nota;
+        cantidad+=1;
+        nota = parseInt(prompt("nota:", ));
+    }
+
+    if(cantidad !== 0) {
+
+        return ((suma/cantidad));
+    }
+    else {
+        return (0);
+    }
+
+}
+console.log(promedio())*/
+
+
+
+
+function siguiente(numero) {
+    return numero+1;
+    
+}
+console.log(siguiente(45));
+
+siguienteFlecha = num => ++num
+console.log(siguienteFlecha(45));
+
+
+/*5. Una función que se llame doble, que reciba como parámetro un valor, y
+devuelva el doble del número ingresado como parámetro.*/
+
+function doble(numero) {
+    return numero*2;
+    
+}
+console.log(doble(18))
+
+var dobleFlecha = num => num*2
+console.log(dobleFlecha(18))
+
+dobleFlecha = num => num*2
+console.log(dobleFlecha(18))
+
+
+/*6. Una función que se llame cuadrado, que reciba como parámetro un
+valor, y devuelva el valor del numero ingresado como parámetro
+elevado al cuadrado.*/
+
+function cuadrado(numero) {
+    return numero**2;
+    
+}
+console.log(cuadrado(9))
+
+var cuadradoFlecha = num => num*num
+console.log(cuadradoFlecha(9))
+
+cuadradoFlecha = num => num*num
+console.log(cuadradoFlecha(9))
+
+/*7. Una función que se denomine imprimirValores que dado un número
+por parámetro, imprima cual es el valor siguiente, el doble y el
+cuadrado. Usando las funciones definidas en los puntos 5) , 6) y 7) :
+siguiente, el doble y el cuadrado.*/
+
+function imprimirValores(numero) {
+    console.log(siguiente(numero));
+    console.log(doble(numero));
+    console.log(cuadrado(numero));
+}
+imprimirValores(7)
+
+
+function imprimirValores(numero) {
+    console.log(siguiente(numero));
+    console.log(doble(numero));
+    console.log(cuadrado(numero));
+}
+imprimirValores(7)
+
+
+
+
+/*8. Una función que se denomine imprimirElDobleDelSiguiente que dado
+un número por parámetro, imprima cual es el valor siguiente al el doble.
+Usando las funciones definidas en los puntos 5) , 6) : siguiente, el
+doble.*/
+
+function imprimirElDobleDelSiguiente(numero) {
+    return doble(siguiente(numero));
+ 
+}
+console.log(imprimirElDobleDelSiguiente(9))
+
+
+/*9. Una función que se denomine
+imprimirElDobleDelSiguienteAlCuadrado que dado un número por
+parámetro, imprima cual es el valor siguiente al el doble. Usando las
+funciones definidas en los puntos 5) , 6) y 7) : siguiente, el doble y el
+cuadrado.*/
+
+function imprimirElDobleDelSiguienteAlCuadrado(numero) {
+    return doble(cuadrado(siguiente(numero)));
+   
+}
+console.log(imprimirElDobleDelSiguienteAlCuadrado(3))
+
+
+
+/*10.Una función que dado la longitud de un lado de un cuadrado devuelva
+el perímetro.*/
+
+function perimetroDelCuadradoDe(lado) {
+    return console.log(lado*4);
+}
+perimetroDelCuadradoDe(4)
+
+
+/*11. Una función que dado la longitud de un lado de un cuadrado devuelva
+la superficie.*/
+
+function superficieDelCuadradoDe(lado) {
+    return console.log(lado**2);
+}
+superficieDelCuadradoDe(10)
+
+
+/*12.Una función que dado el radio de un círculo devuelva la circunferencia
+(perímetro del círculo).*/
+
+function circunferenciaDelCirculoDe(radio) {
+    return console.log(Math.PI*2*radio);
+}
+circunferenciaDelCirculoDe(6)
+
+
+/*13.Una función que dado el radio de un círculo devuelva el área del
+círculo.*/
+
+function areaDelCirculoDe(radio) {
+    return console.log(Math.PI*radio*radio);
+}
+areaDelCirculoDe(4)
+
+function areaDelCirculoDe(radio) {
+    return console.log(Math.PI*radio**2);
+}
+areaDelCirculoDe(4)
+
+
+/*14.Una función que dado un número de mes y me devuelva la cantidad de
+días de ese mes (suponiendo que no es un año bisiesto).*/
+
+function diasDelMes(mes) {
+   if (mes == 2) {
+       return 28;
+   } else {
+    if (mes == 4 || mes == 6 || mes == 9 || mes == 4) {
+        return 30;
+    }
+   }
+   return 31; 
+    
+   }
+console.log(diasDelMes(2)) 
+console.log(diasDelMes(4)) 
+console.log(diasDelMes(12)) 
+
+
+
+/*15.Una función que reciba como parámetro un año y diga si ese año es
+bisiesto.*/
+
+function esAñoBisiesto(año) {
+    if (año%4 == 0) {
+        return true;
+    }
+    return false;
+}
+console.log(esAñoBisiesto(2012))
+console.log(esAñoBisiesto(2013))
+
+/*16.Una función que reciba el día, mes y año y me devuelva la cantidad de
+días de ese mes.*/
+
+function cantDeDias(dia, mes, año) {
+   
+    if (esAñoBisiesto(año)) {
+       
+        return dia+diasDelMes(mes)+366;
+    }
+
+    return dia+diasDelMes(mes)+365;
+}
+console.log(cantDeDias(27, 7, 2012))
+console.log(cantDeDias(27, 7, 2013))
+
+
+// 17.Una función que reciba día, mes y año y devuelva el día anterior
+
+function diaAnterior(dia, mes, año) {
+    return dia-1;
+    
+}
+console.log(diaAnterior(27, 7, 2020),)
+
+
+/* 18.Una función que se llame ultimoDiaDelMes que reciba día, mes y año y
+devuelva el último día del mes.*/
+
+function ultimoDiaDelMes(dia, mes, año) {
+    return diasDelMes(mes);
+}
+console.log(ultimoDiaDelMes(27, 7, 2020))
+
+
+// Tabla de multilicar.
+/*
+function tablaMultiplicar(tabla, hasta) {
+    for (var i = 1; i <= hasta; i++)
+    console.log(tabla + " x " + i + " = ", tabla * i)
+ }
+ tablaMultiplicar(5, 10)*/
+
+
+ /*26.Ejercicio 8: escriba una función de JavaScript que invierta un número.
+Por ejemplo, si x = 32443, la salida debería ser 34423.*/
+
+/*
+console.log("123456".length)
+
+console.log((123456).toString())
+
+var num = (567890).toString()
+var a = num[1]
+console.log(a);*/
+
+
+
+ function invertir(numero) {
+    let num = (numero).toString();
+    let longitud = num.length;
+    let numInvertido = num[longitud-1];
+   for (let i=longitud-2; i>=0; i--) {
+        numInvertido+=num[i];
+    }
+   return parseInt(numInvertido,10);
+}
+
+console.log(invertir(123456))
+console.log(invertir(1111199999))
+console.log(invertir(10101010))
+console.log(invertir(9876543210))
+
+
+/*27.Ejercicio 9: escriba una función de JavaScript que devuelva una cadena
+en orden alfabético. Por ejemplo, si x = 'webmaster', la salida debería
+ser 'abeemrstw'. La puntuación y los números no se pasan en la
+cadena.*/
+
+/*
+function ordenarAlfa(palabra) {
+
+let palabra2;
+let n = palabra.length;
+
+for(let j=1; j<palabra.length; j++) {
+   
+    
+    for(let i=0; i<n; i++) {
+
+        if(palabra[i] >= palabra[i+1]) {
+    
+            palabra2[i] = palabra[i+1];
+            palabra2[i+1] = palabra[i];
+        }
+        i++;
+  
+  }
+  j++;
+  n--;
+  
+
+}
+return console.log(palabra2);
+
+}
+
+ordenarAlfa("virginia")*/
+
+
+
+function ordenarAlfa(palabra) {
+    let palabraOrdenada;
+    let aux;
+    aux = palabra[0];
+
+    for(let i=1; i<palabra.length; i++) {
+        
+        for(let j=1; j<palabra.length; j++) {
+
+            if(aux > palabra[j]) {
+                aux=palabra[j];
+                j++;  
+            }
+            j++;
+        }
+        palabraOrdenada+=aux;
+        i++;
+    }
+    return console.log(palabraOrdenada); 
+}
+
+ordenarAlfa("virginia")
+
+/*28.Ejercicio 10: escriba una función de JavaScript que convierta la primera
+letra de cada palabra a mayúsculas. Por ejemplo, si x = "prince of
+persia", la salida debería ser "Prince Of Persia".*/
+
+/*
+function ejercicio10(parrafo) {
+    let aux;
+    aux=parrafo[0].toUpperCase();
+    let i=1;
+    for(i; 1<=parrafo.length; i++){
+
+       if(parrafo[i] == "") {
+         aux+=parrafo[i+1].toUpperCase();
+       }
+       
+    }
+
+    return aux;
+}
+
+console.log("vamos river");*/
+
+
+/*29.Ejercicio 11: escriba una función de JavaScript que busque la palabra
+más larga de una frase. Por ejemplo, si x = "Tutorial de desarrollo web",
+el resultado debería ser "Desarrollo"*/
